e8b7b6a6c0b68aef9f5da99667c9c9de72c7e762
mostafa osama <mostafaelshrombably775@gmail.com>
2022-11-15 08:09:58 +0200

    stopped tracking .env File

--- common/isAuthoraized.js
+++ common/isAuthoraized.js
@@ -3,7 +3,9 @@ const jwt = require("jsonwebtoken");
 module.exports = () => {
   return (req, res, next) => {
     const token = req.headers.authorization.split(" ")[1];
+
     const decoded = jwt.verify(token, "shhhhh");
+    console.log(decoded);
     req.user = decoded;
     next();
   };
